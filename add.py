bbffab9eabfaa6444d0f8921d6aac99475b0205e
vijayalakshmi999 <35723903+vijayalakshmi999@users.noreply.github.com>
2019-08-27 12:51:43 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,4 @@
+print("viji")
+a=int (input("enter a  value"))
+b=int (input("enter  b value"))
+print (int(a+b))
---
+++ vj(16ug10022).py
@@ -0,0 +1,56 @@
+Python 3.7.3 (default, Apr  3 2019, 05:39:12) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=2
+>>> b=8
+>>> a+b
+10
+>>> a="hello cmr"
+>>> a
+'hello cmr'
+>>> a= "hello"
+>>> b="cmr"
+>>> a+b
+'hellocmr'
+>>> a= "hi "
+>>> b= "student"
+>>> aq+b
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    aq+b
+NameError: name 'aq' is not defined
+>>> a+b
+'hi student'
+>>> a
+'hi '
+>>> split(" ,")
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    split(" ,")
+NameError: name 'split' is not defined
+>>> slipt(',')
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    slipt(',')
+NameError: name 'slipt' is not defined
+>>> a=
+SyntaxError: invalid syntax
+>>> a
+'hi '
+>>> a="hello" "viji"
+>>> b
+'student'
+>>> a=a+b
+>>> a
+'hellovijistudent'
+>>> a="hello, viji, student"
+>>> a
+'hello, viji, student'
+>>> a.split(",")
+['hello', ' viji', ' student']
+>>> 
+['hello', ' viji', ' student']
+['hello', ' viji', ' student']
+>>> a.replace("hello","fn")
+'fn, viji, student'
+>>> 
