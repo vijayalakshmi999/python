85a4fb1d341323e4e416c1004fb5974d3d36bcd6
vijayalakshmi999 <35723903+vijayalakshmi999@users.noreply.github.com>
2019-09-03 12:27:59 +0530

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,11 @@
+my_dict ={'name': 'viji','age': 19, 'address': 'bellary'}
+print(my_dict['name'])
+print(my_dict['age'])
+print(my_dict['address'])
+# empty dictionary
+squares = {1:1,1:2,2:4,3:9,4:16,5:25,6:36,7:49}
+print(squares)
+print(squares.pop(2))
+print(squares.popitem())
+print(squares)
+del squares[3]
